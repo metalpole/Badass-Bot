40e8d903f32bd0f796b8c9c66a23e2fd009484a1
metalpole <36705536+metalpole@users.noreply.github.com>
2018-07-13 15:04:32 +0800

    Update introbot2.py

--- introbot2.py
+++ introbot2.py
@@ -11,18 +11,15 @@ URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 greetings = {1: "I hear you 110% man", 2: "I'm listening", 3: "I feel you bro",
              4: "Go on", 5: "Hmmm", 6: "yup"}
 
-
 def get_url(url):
     response = requests.get(url)
     content = response.content.decode("utf8")
     return content
 
-
 def get_json_from_url(url):
     content = get_url(url)
     js = json.loads(content)
     return js
-
 
 def get_updates(offset=None):
     url = URL + "getUpdates?timeout=100"
@@ -37,19 +34,23 @@ def get_last_update_id(updates):
         update_ids.append(int(update["update_id"]))
     return max(update_ids)
 
-def echo_all(updates):
+def handle_updates(updates):
     for update in updates["result"]:
         try:
             text = update["message"]["text"]
             chat = update["message"]["chat"]["id"]
-            if "eden" in text.lower():
-                send_message("Thank you so much for your donation of ETH. The EdenChain team wishes you all the best in trying to get a refund of your ETH deposit. Do learn to put your remaining fiat to better use. Cheers!", chat)
-            elif len(text) > 20:
-                send_message(greetings[random.randint(1,5)], chat)
-        except Exception as e:
-            print(e)
-
-
+            items = db.get_items()
+            if text in items:
+                db.delete_item(text)
+                items = db.get_items()
+            else:
+                db.add_item(text)
+                items = db.get_items()
+            message = "\n".join(items)
+            send_message(message, chat)
+        except KeyError:
+            pass
+  
 def get_last_chat_id_and_text(updates):
     num_updates = len(updates["result"])
     last_update = num_updates - 1
@@ -57,13 +58,11 @@ def get_last_chat_id_and_text(updates):
     chat_id = updates["result"][last_update]["message"]["chat"]["id"]
     return (text, chat_id)
 
-
 def send_message(text, chat_id):
     text = urllib.parse.quote_plus(text)
     url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     get_url(url)
     
-
 def main():
     last_update_id = None
     while True:
@@ -72,7 +71,6 @@ def main():
             last_update_id = get_last_update_id(updates) + 1
             echo_all(updates)
         time.sleep(3)
-
 
 if __name__ == '__main__':
     main()
